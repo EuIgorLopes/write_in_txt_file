f90470a0f9def8da0662c9361af39da4e027af10
igorLopes <igor.lopes@mdisys.pw>
2018-02-08 01:39:59 -0200

    add comentes en and readme.txt

---
+++ readme.txt
@@ -0,0 +1,3 @@
+This is a simple code for write in txt files with line broke.
+
+The reserved constant PHP_EOL is usage for solution the line broke.
--- txt_file.class.php
+++ txt_file.class.php
@@ -4,15 +4,24 @@
 			/* 
 				pt_br: Abre o arquivo para escrita com o tipo de acesso 'a'. O arquivo será gerado caso não exista.
 					Mais informações em: http://php.net/manual/pt_BR/function.fopen.php
+
+				en: Open the file write with type access 'a'. The file is generated if not exist
+					More information in: http://php.net/manual/en/function.fopen.php
 			*/
 			$file = fopen($file_name, 'a');
 
 			/*
 				pt_br: Escreve o conteúdo de $text no arquivo.
-					PHP_EOL é uma constante reservada do PHP. É o simbolo correto de fim de linha (também utilizado para quebra de linhas em arquivos).
+					PHP_EOL é uma constante reservada do PHP. É o simbolo correto de "fim de linha" (também utilizado para quebra de linhas em arquivos).
 					Mais informações em:
 					http://php.net/manual/pt_BR/function.fwrite.php
 					https://secure.php.net/manual/pt_BR/reserved.constants.php
+
+				en: Write the content of $text in file.
+					PHP_EOL is a reserved constant of PHP. Is the correct symbol of "end of line" (also use for line broke in files).
+					More information in:
+					http://php.net/manual/en/function.fwrite.php
+					http://php.net/manual/en/reserved.constants.php
 			*/
 			fwrite($file, $text.PHP_EOL);
 
@@ -20,6 +29,8 @@
 				pt_br: Fecha o ponteiro do arquivo que está sendo escrito.
 					Mais informações em: http://php.net/manual/pt_BR/function.fclose.php
 
+				en: Close the pointer of file being written.
+					More information in: http://php.net/manual/en/function.fclose.php
 			*/
 			fclose($file);
 		}
